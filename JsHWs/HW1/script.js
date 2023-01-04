604d872199f032bce34a199518a87cc76fb49854
RostyslavSyniuk <rostyslavsyniuk@gmail.com>
2023-01-04 22:39:10 +0200

    first js home work commit

---
+++ JsHWs/HW1/script.js
@@ -0,0 +1,83 @@
+// Напиши скрипт, який розраховує індекс маси тіла людини. Для цього необхідно розділити вагу в кілограмах на квадрат висоти людини за метри.
+// let weight = '88.3';
+// let height = '1.75';
+
+// const bmi = weight / height**2
+// console.log(bmi); // 28.8
+
+
+
+//-----------------------------------
+
+// Напиши скрипт, який виводить у консоль заокруглені вгору/вниз і т.д. Значення змінної value.
+// Використовуй методи Math.floor(), Math.ceil() та Math.round(). Перевірте, що буде в консолі при значеннях 27.3 і 27.9.
+// let value = 27.3
+// console.log(Math.floor(value))
+
+// console.log(Math.ceil(value))
+
+// console.log(Math.round(value))
+
+// value = 27.9
+// console.log(Math.floor(value))
+
+// console.log(Math.ceil(value))
+
+// console.log(Math.round(value))
+// -----------------------------------
+
+// Напиши скрипт, який переведе значення totalMinutes (кількість хвилин) у рядок у форматі годин і хвилин HH:MM.
+
+// 70 покаже 01:10
+// 450 покаже 07:30
+// 1441 покаже 24:01
+
+// const totalMinutes = 450;
+// let minutes = totalMinutes % 60
+// let hours = Math.floor(totalMinutes/ 60) 
+// let formatted = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0');
+
+// console.log(formatted)
+
+
+//-----------------------------------
+
+/* 
+ * Вивести на екран модальне вікно prompt з повідомленням "Enter the number".
+ * Результат виконання модального вікна записати в змінну, значення якої вивести в консоль.
+ * */
+
+// let result = prompt('Enter the number');
+
+// console.log(result)
+//-----------------------------------
+
+/* 
+   * За допомогою модального вікна prompt отримати від користувача два числа.
+   * Вивести в консоль суму, різницю, добуток, поділ та залишок від поділу їх один на одного.
+   * */
+
+// let result1 = prompt();
+// let result2 = prompt();
+
+// console.log(Number(result1) + Number(result2))
+
+// let result1 = prompt();
+// let result2 = prompt();
+
+// console.log(Number(result1) - Number(result2))
+
+// let result1 = prompt();
+// let result2 = prompt();
+
+// console.log(Number(result1) * Number(result2))
+
+// let result1 = prompt();
+// let result2 = prompt();
+
+// console.log(Number(result1) / Number(result2))
+
+// let result1 = prompt();
+// let result2 = prompt();
+
+// console.log(Number(result1) % Number(result2))
